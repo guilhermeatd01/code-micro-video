7edc8970e20ae44cd9b54548d325b4d521a83566
Guilherme Fonseca <guilherme.atd@hotmail.com>
2021-02-28 13:35:46 -0300

    Add cloudbuild configuration

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,6 +2,8 @@
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 chown -R www-data:www-data .
+cp .env.testing.example .env.testing
+cp .env.example .env
 composer install
 php artisan key:generate
 php artisan migrate
