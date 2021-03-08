b027ea0b2fe5128c2f65a711152b5b07c167cbf8
Guilherme Fonseca <guilherme.atd@hotmail.com>
2021-03-08 08:51:49 -0300

    Add resource Video

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Genre;
 use Illuminate\Database\Seeder;
 
@@ -14,8 +15,13 @@ class GenreSeeder extends Seeder
      */
     public function run()
     {
+        $categories = Category::all();
         Genre::factory()
                     ->count(10)
-                    ->create();
+                    ->create()
+                    ->each(function(Genre $genre) use ($categories) {
+                        $categoriesId = $categories->random(5)->pluck('id')->toArray();
+                        $genre->categories()->attach($categoriesId);
+                    });
     }
 }
